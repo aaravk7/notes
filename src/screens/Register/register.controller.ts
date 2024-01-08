12a558a596c023dc139b41aa5e3cfe39f65a7fab
Aarav <aaravkhokhar@yahoo.in>
2024-01-08 21:13:24 +0530

    fix: fix urls

--- src/screens/Register/register.controller.ts
+++ src/screens/Register/register.controller.ts
@@ -5,13 +5,16 @@ export interface RegisterArgs {
 }
 
 export async function register(args: RegisterArgs) {
-  const response = await fetch("http://localhost:5000/auth/register", {
-    method: "POST",
-    body: JSON.stringify(args),
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  const response = await fetch(
+    import.meta.env.VITE_SERVER_URL + "/auth/register",
+    {
+      method: "POST",
+      body: JSON.stringify(args),
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
   const data = await response.json();
   if (response.status !== 200) {
     throw new Error(data.error ?? "Something went wrong");
